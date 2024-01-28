a4ad5e7ae86c763f0f85aad260cf970a3eab38a7
Muhammadrizo28 <muhammadrizo281@gmail.com>
2024-01-28 15:44:26 +0300

    first part

---
+++ script.js
@@ -0,0 +1,18 @@
+let name = 'Alex'
+
+let account = 7777
+
+let money = 10000
+
+let ask = prompt('Name ?')
+
+if (ask == name) {
+    let ask2 = prompt('Account ?')
+
+    if (ask2 == account) {
+        
+    }
+}
+else {
+    alert('error')
+}
